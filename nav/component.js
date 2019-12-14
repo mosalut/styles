4d5d91f691dad431c6cb9421e99fd5350462b314
mosalut <ludaoyuan@mosalut.com>
2019-12-14 18:23:41 +0800

    添加了menu 和 menuItem
    On branch mosalut
    Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	new file:   nav/component.js
    	new file:   nav/style.css
    	new file:   nav/template.html
    	new file:   style.css
    	modified:   test.html
    	modified:   test.js

---
+++ nav/component.js
@@ -0,0 +1,34 @@
+"use strict";
+
+export function nav_init() {
+	let imports = document.querySelectorAll('#mosalut-nav[rel="import"]');
+	let fs;
+	let fItems;
+	for(let i = 0; i < imports.length; i++) {
+		let menu = imports[i].import.getElementById('menu').content;
+		fs = document.getElementsByTagName('mosalut-menu');
+		for(let j = 0; j < fs.length; j++) {
+			let clone = document.importNode(menu, true)
+			let html = fs[j].innerHTML;
+			let title = fs[j].dataset.title;
+			console.log(title);
+			clone.querySelector("header>h4").innerHTML = title;
+			clone.querySelector("section").innerHTML = html;
+			fs[j].innerHTML = '';
+			fs[j].appendChild(clone);
+		}
+
+		if(fs.length != 0)
+			continue;
+
+		let menuItem = imports[i].import.getElementById('menuitem').content;
+		fItems = document.getElementsByTagName('mosalut-menuitem');
+		for(let j = 0; j < fItems.length; j++) {
+			let clone = document.importNode(menuItem, true)
+			let html = fItems[j].innerHTML;
+			clone.innerHTML = html;
+			fItems[j].innerHTML = '';
+			fItems[j].appendChild(clone);
+		}
+	}
+}
--- test.js
+++ test.js
@@ -2,10 +2,12 @@
 
 import {window_init} from './window/component.js';
 import {fieldset_init} from './fieldset/component.js';
+import {nav_init} from './nav/component.js';
 
 function init() {
 	window_init();
 	fieldset_init();
+	nav_init();
 }
 
 function main() {
